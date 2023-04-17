9c77cea064149fae99f99704c0e57c9e719df980
Sarawutgame <63070165@kmitl.ac.th>
2023-04-17 20:15:45 +0700

    Add Upload image

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import GroupPage from './grouppost/listgroup';
+import GroupPage from './grouppost/listgroup';
 // import Group from './grouppost/grouppage';
 // import Lostdetail from './lostpet/lostdetail';
 // import Group from './grouppost/grouppage';
@@ -11,8 +11,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    {/* <GroupPage/> */}
+    {/* <App /> */}
+    <GroupPage/>
     {/* <Group/> */}
   </React.StrictMode>
 );
